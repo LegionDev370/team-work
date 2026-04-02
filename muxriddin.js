20a53d059b07ad9173267fce497db2d989c74975
berdullayev@gmail.com <macbookpro@MACBOOKnoMacBook-Pro.local>
2026-04-02 11:48:12 +0500

    mm

--- muxriddin.js
+++ muxriddin.js
@@ -1,13 +1,15 @@
- let sentence34 ="The quick brown fox jumps over the lazy dog";
-function getAcronym(sentence){
-    sentence = sentence.split(" ");
-    const toUpper = sentence.map(el => {
-        return el.charAt(0).toUpperCase();
-    });
-    let result = String(toUpper.map(el => {
-        return el;
-    }));
-    result = result.split(",").join(",");
-    console.log(result);
-};
+let sentence34 = "The quick brown fox jumps over the lazy dog";
+function getAcronym(sentence) {
+  sentence = sentence.split(" ");
+  const toUpper = sentence.map((el) => {
+    return el.charAt(0).toUpperCase();
+  });
+  let result = String(
+    toUpper.map((el) => {
+      return el;
+    }),
+  );
+  result = result.split(",").join(",");
+  console.log(result);
+}
 getAcronym(sentence34);
